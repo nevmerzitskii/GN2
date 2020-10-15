e208980e2a5f93daca64e5ade0896f52f11a50b9
nevmerzitskii <nikitanevmerzitskii@gmail.com>
2020-10-15 19:19:34 +0300

    ---

--- main.py
+++ main.py
@@ -14,8 +14,28 @@ pygame.display.set_caption('Угадай число')
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((W, H))
 
+bg = pygame.image.load('image/room.png')
+bg_rect = bg.get_rect(topleft=(0, 0))
+cat = pygame.image.load('image/cat.png')
+cat_rect = cat.get_rect(center=(70, 220))
+dog = pygame.image.load('image/dog.png')
+dog_rect = dog.get_rect(center=(410, 220))
+owl = pygame.image.load('image/owl.png')
+owl_rect = owl.get_rect(center=(210, 120))
+dialog = pygame.image.load('image/dialog.png')
+dialog_rect = dialog.get_rect()
+
 run = True
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             run = False
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+               run = False
+
+    screen.blit(bg, bg_rect)
+    screen.blit(cat, cat_rect)
+    screen.blit(dog, dog_rect)
+    screen.blit(owl, owl_rect)
+    pygame.display.update()

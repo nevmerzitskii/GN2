f8d13728a7c67cd6b0d1bb1b92b8e89022b99e5e
nevmerzitskii <nikitanevmerzitskii@gmail.com>
2020-10-22 19:08:01 +0300

    wat

--- main.py
+++ main.py
@@ -28,10 +28,11 @@ dog_rect = dog.get_rect(center=(410, 220))
 owl = pygame.image.load('image/owl.png')
 owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load('image/dialog.png')
+
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
-dialog_dog_pos = (dog_rect.x - dialog_rect.w// 2, dog_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.W // 2, dog_rect.y - dialog_rect.h)
 
 font = pygame.font.SysFont('Arial', 28, True, False)
 font2 = pygame.font.SysFont('Arial', 14, True, False)
@@ -39,11 +40,11 @@ font_box = pygame.Surface((W - 30, font.get_height()))
 font_box_rect = font_box.get_rect(center=(W // 2, H - 30))
 
 
-def dialogs(text, pos, owl_text)
-    screen.blit(dialog, pos)
-    screen.blit(font2.render(text. True, BLACK), (pos[0] + 5, pos[1] + 5))
+def dialogs(text, pos, owl_pos, owl_text):
+    screen.blit(dialog_pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
     screen.blit(dialog_owl_pos)
-    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos:[1] + 5))
     pygame.display.update()
     pygame.time.wait(2000)
 
@@ -56,12 +57,19 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_ESCAPE:
                run = False
-
-
+               
     if block == 0:
         screen.blit(bg, bg_rect)
         screen.blit(cat, cat_rect)
         screen.blit(dog, dog_rect)
         screen.blit(owl, owl_rect)
         screen.blit(font_box, font_box_rect)
-    font_box.fill(SILVER)
+        font_box.fill(SILVER)
+    pygame.display.update()
+
+    if start == 1:
+        dialogs('', OUTSIZE_BG, dialog_owl_pos, 'я загадала число')
+        dialogs('', OUTSIZE_BG, dialog_owl_pos 'от 0 до 100')
+        dialogs('', OUTSIZE_BG, dialog_owl_pos, 'отгадайте его')
+        dialogs('Кот, твой ход', dialog_dog_pos, OUTSIZE_BG '')
+        start = 0
